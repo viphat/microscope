7318b682e5feac4d6822be68ee91b7668fd099c2
Eddie Yang <viphat@gmail.com>
2015-06-24 22:51:56 +0700

    Done Chapter 8

--- client/templates/posts/post_submit.js
+++ client/templates/posts/post_submit.js
@@ -6,8 +6,16 @@ Template.postSubmit.events({
       title: $(e.target).find('[name=title]').val()
     };
 
-    post._id = Posts.insert(post);
-    Router.go('postPage', post);
+    Meteor.call('postInsert', post, function(error, result) {
+      if (error) {
+        return alert(error.reason);
+      }
 
+      if (result.postExists) {
+        alert('This link has already been posted');
+      }
+
+      Router.go('postPage', {_id: result._id});
+    });
   }
 });
--- client/templates/posts/posts_list.js
+++ client/templates/posts/posts_list.js
@@ -1,5 +1,6 @@
 Template.postsList.helpers({
-  posts: function(){
-    return Posts.find();
+  posts: function() {
+    return Posts.find({}, {sort: {submitted: -1}});
   }
+
 });
